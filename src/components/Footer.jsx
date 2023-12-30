890a5348faf30145ab5de40652e42f76318ad79b
pabledo <pablo_m_c0@hotmail.com>
2023-12-30 19:14:56 -0300

    Styling main components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,36 @@
 export const Footer = () => {
-  return (
-    <footer>
-        
-    </footer>
-  )
+	return (
+		<footer className="grid lg:grid-cols-4 grid-cols-1 gap-x-14 bg-primary lg:p-24 p-4">
+			<div className="flex justify-center items-center">
+				<img className="lg:w-auto w-72 lg:h-fit" src="./images/logoLarge.jpg" alt="little lemon logo" />
+			</div>
+			<div className="lg:ms-12">
+				<ul className="lg:text-start text-center lg:mb-auto mb-6">
+					<li><p className="font-bold">Links</p></li>
+					<li><a href="">Home</a></li>
+					<li><a href="">About</a></li>
+					<li><a href="">Menu</a></li>
+					<li><a href="">Reservations</a></li>
+					<li><a href="">Order Online</a></li>
+					<li><a href="">Login</a></li>
+				</ul>
+			</div>
+			<div className="lg:ms-7">
+				<ul className="lg:text-start text-center lg:mb-auto mb-6">
+					<li><p className="font-bold">Contact</p></li>
+					<li><a href="">5723 S Damen Ave <br /> Chicago, Illinois(IL), 60636</a></li>
+					<li><a href="">(123) 456-7890</a></li>
+					<li><a href="">contact@littlelemon.com</a></li>
+				</ul>
+			</div>
+			<div className="lg:ms-7">
+				<ul className="lg:text-start text-center lg:mb-auto mb-6">
+					<li><p className="font-bold">Social Media Links</p></li>
+					<li><a href="">Facebook</a></li>
+					<li><a href="">Twitter</a></li>
+					<li><a href="">Instagram</a></li>
+				</ul>
+			</div>
+		</footer>
+	)
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,9 @@
 
 export const Nav = () => {
     return (
-        <nav className="flex justify-center gap-10">
-            <img  src={"../images/logo.jpg"} alt="little lemon logo" />
-            <ul className="flex justify-center gap-5">
+        <nav className="flex justify-evenly items-center gap-12 bg-white p-5">
+            <img className="w-40 h-auto" src={"./images/logo.jpg"} alt="little lemon logo" />
+            <ul className="flex justify-center gap-5 font-bold">
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
                 <li><a href="">Menu</a></li>
